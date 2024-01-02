e1e23430775383c3b42a802c0887665e45e035fc
LoafOrc <ltr378@gmail.com>
2024-01-02 19:08:30 +1000

    Patch into PlayerControllerB to add to pause menu

---
+++ Patches/PlayerController_Patches.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameNetcodeStuff;
+using UnityEngine;
+
+namespace LethalSettings.Patches;
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+internal class PlayerController_Patches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(PlayerControllerB.Start))]
+    static void Start_Postfix(PlayerControllerB __instance)
+    {
+        var settingsContainer = GameObject.Find("Systems/UI/Canvas/QuickMenu/SettingsPanel").transform; // FIXME: This probably isn't the best
+        var menu = GameObject.Instantiate(Assets.ModSettingsView, settingsContainer);
+        menu.transform.SetSiblingIndex(menu.transform.GetSiblingIndex() - 2);
+    }
+}
