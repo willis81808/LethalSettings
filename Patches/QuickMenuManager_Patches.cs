7ff8b50dc3bd5a1a823e3f7b8d292cb7a90494ef
willis81808 <willis81808@gmail.com>
2024-01-02 19:43:21 -0700

    added QuickMenuManager patch to inject mod settings menu into the in-game quick menu

---
+++ Patches/QuickMenuManager_Patches.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LethalSettings.Patches;
+
+[HarmonyPatch(typeof(QuickMenuManager))]
+internal class QuickMenuManager_Patches
+{
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(QuickMenuManager.Start))]
+    static void Start_Postfix(QuickMenuManager __instance)
+    {
+        var settingsContainer = __instance.menuContainer.transform.Find("SettingsPanel");
+        var menu = GameObject.Instantiate(Assets.ModSettingsView, settingsContainer);
+        menu.InGame = true;
+        menu.transform.SetSiblingIndex(menu.transform.GetSiblingIndex() - 2);
+    }
+}
